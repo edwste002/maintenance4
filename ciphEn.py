dafa2fab6d912f379e78a148fe51e4209032e5ee
edwste002 <117501584+edwste002@users.noreply.github.com>
2024-03-04 20:41:22 -0500

    Seems to work.  Works on folder.

--- ciphEn.py
+++ ciphEn.py
@@ -4,19 +4,50 @@ import os
 import json
 import sys
 
-key = "test key"
-#-------------------- algo
+#----------------------look at later---??
+def get_header_string_val_from_bytes_in_array(t,i):
+    l = []
+    while t[i]!=0:
+        #print('-----------------------',sf[i])
+        #print(type(sf[i]))
+        #chr(sf[i])
+        #print('33')
+        l.append(chr(t[i]))
+        i = i + 1
+    i = i + 1
+    #print(l)
+    return int(''.join(l)),i
 
-import math as mp
-import inspect
-import os
-import json
-import sys
-
+#------------------------------------------data functions
 key = "test key"
 bits32 = [ i for i in range(32)]
 bits32xor = [ str(i % 2) for i in range(32) ]
-bits128xor = [ str(i % 2) for i in range(128) ] 
+bits128xor = [ str(i % 2) for i in range(128) ]
+
+alphalist=[chr(i) for i in range(256)]
+
+def array_index(arr, indices):
+    if len(indices)==0:
+        return arr
+    return multiget_rec(arr[indices[0]], indices[1:])
+
+def bitstring_to_bytes(s):
+    return int(s, 2).to_bytes((len(s) + 7) // 8, byteorder='little')
+
+def getbinval(v):
+    val = alphalist.index(v)
+    v = bin(val)[2:]
+    b = '0'*(8-len(v)) + v
+    return b
+
+def getbytebinval(v):
+    v = bin(v)[2:]
+    b = '0'*(8-len(v)) + v
+    return b
+
+def gettextval(v):
+    return alphalist[int("0b"+"".join(v),2)]
+
 
 def diff(fileList, folder1, folder2):
     for f in fileList:
@@ -91,8 +122,10 @@ def getBytesFromArbitraryInteger(i):
     #processes bytes in order left to right
     sb = bin(i)[2:]
     extra = len(sb) % 8
-    sb = '0' * (8 - len(sb)) + sb
+
+    sb = '0' * (8 - extra) + sb
     l = []
+    print('len of bytes from arb integer:',len(sb))
     for i in range(len(sb)//8):
         l.append(int(sb[i*8:(i*8)+8],2) )
     bs = bytes(l)
@@ -151,6 +184,8 @@ def TruffleShuffle128FromBitstring(bitstring128):
         xor_bits = xor_32_bits(bitstring128[( i * 4*8 ) :  (i * 4 * 8) + 4 * 8 ], bits32xor)
         l.append(xor_bits)
     return int(''.join( (l[i] for i in range(4) ) ) , base=2)
+
+#------------------------------------------------------derangement functions
 
 derangementSize = [1, 0, 1, 2, 9, 44, 265, 1854, 14833, 133496, 1334961, 14684570, 176214841, 2290792932, 32071101049, 481066515734, 7697064251745, 130850092279664, 2355301661033953, 44750731559645106, 895014631192902121, 18795307255050944540, 413496759611120779881, 9510425471055777937262, 228250211305338670494289, 5706255282633466762357224, 148362637348470135821287825, 4005791208408693667174771274, 112162153835443422680893595673, 3252702461227859257745914274516, 97581073836835777732377428235481, 3025013288941909109703700275299910, 96800425246141091510518408809597121, 3194414033122656019847107490716704992, 108610077126170304674801654684367969729, 3801352699415960663618057913952878940514, 136848697178974583890250084902303641858505, 5063401795622059603939253141385234748764684, 192409268233638264949691619372638920453057993, 7503961461111892333037973155532917897669261726]
 
@@ -217,8 +252,24 @@ def recurseDerangement(i,iterlist,n):
         returnediterlist.insert(firstPart+1,derangement_item[1])
         return returnediterlist
 
-#--------------------------------------end of derangements----------------------------------------------------------------------
-    
+
+#-------------------------------------round function primitives------------------
+
+def selectorShuffle( oldSelector, selector128, extra=''):
+    # TODO add extra later
+    # left shift
+    # xor remainder back on back
+    oldSelector = oldSelector [4:] + oldSelector[0:4]
+    #print(oldSelector)
+    oldSelector = xor128BitsFromBytes(oldSelector, selector128)
+    #print(oldSelector)
+    oldSelector = bytes( int(oldSelector[(i*8):(i+1)*8],2) for i in range(16) )
+    oldSelector = oldSelector [4:] + oldSelector[0:4]
+    #print(len(oldSelector))
+    oldSelector = xor128BitsFromBytes(oldSelector, selector128)
+    oldSelector = bytes( int(oldSelector[i*8:(i+1)*8],2) for i in range(16) ) 
+    return oldSelector
+
 def eee_round(bitsInSelectorFunction128bitsFromBytes, bitsIn32bitsAsBytes, count=0):
     #print('bits:', bitsIn32bitsAsBytes)
     b32 =  get32bitstringFromBytes(bitsIn32bitsAsBytes)
@@ -266,40 +317,45 @@ def reverse_eee_round(bitsInSelectorFunction128bitsFromBytes, bitsIn32bitsAsByte
     #print(b)
     return b
 
-def get_quick_checksum(data_in):
-    v = data_in[0]
-    for i in range(len(data_in[0:-4])):
-        v = eee_round(data_in[i+0]*(2**(96)) + data_in[i+1]*(2**(64)) + data_in[i+2]*(2**(32)) + data_in[i+3], v )
-        v = int(v,base=2)
-    return eee_round(data_in[-4]*(2**(96)) + data_in[-3]*(2**(64)) + data_in[-2]*(2**(32)) + data_in[-1], v )
+#----------------------------------------------------round functions
 
+def encrypt_rounds(oldSelector,predata, data):
+    d = predata[0:16] + data[:] + predata[0:16] + predata[0:16]
+    output = bytes()
 
-def selectorShuffle( oldSelector, selector128, extra=''):
-    # TODO add extra later
-    # left shift
-    # xor remainder back on back
-    oldSelector = oldSelector [4:] + oldSelector[0:4]
-    #print(oldSelector)
-    oldSelector = xor128BitsFromBytes(oldSelector, selector128)
-    #print(oldSelector)
-    oldSelector = bytes( int(oldSelector[(i*8):(i+1)*8],2) for i in range(16) )
-    oldSelector = oldSelector [4:] + oldSelector[0:4]
-    #print(len(oldSelector))
-    oldSelector = xor128BitsFromBytes(oldSelector, selector128)
-    oldSelector = bytes( int(oldSelector[i*8:(i+1)*8],2) for i in range(16) ) 
-    return oldSelector
+    for i in range((len(data)//4)+1):
+        selector128 = d[ (i) * 4 : ((i)*4) + 4*4 ]
+        #--TODO extra does nothing ----------------
+        oldSelector = selectorShuffle( oldSelector, selector128 )
+                
+        data32 = d[ ((i)*4) + 4*4 : ((i+1)*4) + 4*4 ]
+        
+        b = eee_round( oldSelector, data32  , i )
+        
+        output = output + b
+        #print('--->oldselector\t:', oldSelector, 'round\t selector128:',selector128)
+        #print('\t','data32:',data32, '\toutput:\t', output)
+    return oldSelector, output
 
-def process_password(password):
-    #password first
-    #then data
-    if len(password)<20:
-        print("password too short")
-        raise
+def decrypt_rounds(oldSelector,predata,data):
+    d = predata[0:16] + data[:] + predata[0:16] + predata[0:16]
+    output = bytes()
+    #print('data length in bytes:-------------------------------------------------------',(len(data)//4)+1)
 
-    oldSelector = bytes([ 0 for i in range(16) ] )
-    
-    oldSelector, notused = encrypt_rounds(oldSelector,password[0:16], password)
-    return oldSelector, notused
+    for i in range((len(data)//4)+1):
+        selector128 = d[ (i) * 4 : ((i)*4) + 4*4 ]
+        #--TODO extra does nothing ----------------
+        oldSelector = selectorShuffle( oldSelector, selector128 )
+        
+        data32 = d[ ((i)*4) + 4*4 : ((i+1)*4) + 4*4 ]
+        
+        b = reverse_eee_round( oldSelector, data32  , i )
+        d = d[:(i)*4 + 4*4] +  b + d[((i+1)*4) + 4*4:]
+        #print('--->oldselector\t:', oldSelector, 'round\t selector128:',selector128)
+        #print(i)
+        #print('\t','data32:',data32, '\toutput:\t', d)
+    output = d[16:-32]
+    return oldSelector, output
 
 def decrypt_rounds_for_text_size(oldSelector,predata, data):
     d = predata[0:16] + data[:] + predata[0:16]
@@ -318,47 +374,25 @@ def decrypt_rounds_for_text_size(oldSelector,predata, data):
         
     return getArbitraryIntegerFromBytesForTextLength(d[16:32])
 
-def encrypt_rounds(oldSelector,predata, data):
-    d = predata[0:16] + data[:] + predata[0:16]
-    output = bytes()
-    for i in range((len(data)//4)+1):
-        selector128 = d[ (i) * 4 : ((i)*4) + 4*4 ]
-        #--TODO extra does nothing ----------------
-        oldSelector = selectorShuffle( oldSelector, selector128 )
-                
-        data32 = d[ ((i)*4) + 4*4 : ((i+1)*4) + 4*4 ]
-        
-        b = eee_round( oldSelector, data32  , i )
-        
-        output = output + b
-        #print('--->oldselector\t:', oldSelector, 'round\t selector128:',selector128)
-        #print('\t','data32:',data32, '\toutput:\t', output)
-    return oldSelector, output
+#-------------------------------------------non primitive data functions
 
-def decrypt_rounds(oldSelector,predata,data):
-    d = predata[0:16] + data[:] + predata[0:16]
-    output = bytes()
-    print('data length:-------------------------------------------------------',(len(data)//4)+1)
-    for i in range((len(data)//4)+1):
-        selector128 = d[ (i) * 4 : ((i)*4) + 4*4 ]
-        #--TODO extra does nothing ----------------
-        oldSelector = selectorShuffle( oldSelector, selector128 )
-        
-        data32 = d[ ((i)*4) + 4*4 : ((i+1)*4) + 4*4 ]
-        
-        b = reverse_eee_round( oldSelector, data32  , i )
-        d = d[:(i)*4 + 4*4] +  b + d[((i+1)*4) + 4*4:]
-        #print('--->oldselector\t:', oldSelector, 'round\t selector128:',selector128)
-        #print(i)
-        #print('\t','data32:',data32, '\toutput:\t', output)
-    output = d[16:-16]
-    return oldSelector, output
+def process_password(password):
+    #password first
+    #then data
+    if len(password)<20:
+        print("password too short")
+        raise
+
+    oldSelector = bytes([ 0 for i in range(16) ] )
+    
+    oldSelector, notused = encrypt_rounds(oldSelector,password[0:16], password)
+    return oldSelector
 
 def encrypt_data(data,password):
     #predata needs 16 length to work
     #password first
     #then data
-    oldSelector, notused = process_password(password)
+    oldSelector = process_password(password)
     outputsizetext = bytes([])
     outputtext = bytes([])
 
@@ -369,33 +403,25 @@ def encrypt_data(data,password):
     if len(szBytes)>16:
         print('szOfFile too big')
     szBytes = szBytes + (16 - len(szBytes)) * bytes([0])
-    #print('szBytes:', szBytes)
-    #print('length of szBytes:', len(szBytes))
+    print('length of data', len(data))
+    print('szBytes:', szBytes)
+    print('length of szBytes:', len(szBytes))
 
     oldSelector, outputText = encrypt_rounds(oldSelector,password[0:16], szBytes + data)
 
     return outputText
 
-
-
 def decrypt_data(data,password):
     output = bytes()
-    oldSelector, notused = process_password(password)
+    oldSelector = process_password(password)
 
     print('--------------------------------------decrypt for text size oldSelector: ', oldSelector) 
     #limits text size by 2**(8*100)
     sizeOfText = decrypt_rounds_for_text_size(oldSelector,password[0:16], data[0:64])
     print('\tsizeOfText: ', sizeOfText)
-    oldSelector, outputText = decrypt_rounds(oldSelector,password[0:16], data[0:32+sizeOfText])
+    oldSelector, outputText = decrypt_rounds(oldSelector,password[0:16], data)
     print('outputText: ',outputText)
     return outputText[16:16 + sizeOfText]
-
-
-def array_index(arr, indices):
-    if len(indices)==0:
-        return arr
-    return multiget_rec(arr[indices[0]], indices[1:])
-
 
 def get_files_directory(startpath,remove_prefix=''):
     #TODO---------------------------------------------------------  text
@@ -430,24 +456,6 @@ def get_files_directory(startpath,remove_prefix=''):
             
     return [listOfFiles,dirpaths]
 
-alphalist=[chr(i) for i in range(256)]
-
-def bitstring_to_bytes(s):
-    return int(s, 2).to_bytes((len(s) + 7) // 8, byteorder='little')
-
-def getbinval(v):
-    val = alphalist.index(v)
-    v = bin(val)[2:]
-    b = '0'*(8-len(v)) + v
-    return b
-
-def getbytebinval(v):
-    v = bin(v)[2:]
-    b = '0'*(8-len(v)) + v
-    return b
-
-def gettextval(v):
-    return alphalist[int("0b"+"".join(v),2)]
 
 def text_decrypt(text,key):
     data = decrypt_data(text,key)
@@ -464,6 +472,53 @@ def text_encrypt(text,key):
 def text_encrypt_from_bytes_for_header(text,key):
     data = encrypt_data(text,key)
     return data
+
+#-----------------------------------------------------------------------------file based functions
+def decrypt_file_from_storage(storage_file,prefix,relativepath,start,crypted_size,key):
+    #split
+    import os
+    #print(filename)
+    #print(os.path.dirname(filename))
+    print('prefix:\t',prefix)
+    print('path:\t',relativepath)
+    print('start:\t',start)
+    print('crypted_size\t',crypted_size)
+    filename = os.path.join(prefix,relativepath)
+    print('filename\t',filename)
+    try:
+        os.makedirs(os.path.dirname(filename))
+    except FileExistsError:
+        print("File already exists",filename)
+        pass
+    except FileNotFoundError:
+        print("File Not Found error",filename)
+        pass
+    f = open(storage_file,'rb')
+    f.seek(start)
+    data = f.read(crypted_size)
+    
+    out = decrypt_data(data,key)
+    print('-------------------------decrypted file:', relativepath, '----------------------------------------------')
+    print('unencrypted data',data)
+    print('start:',start)
+    #print('size:',sz)
+    print('crypted_size',crypted_size)
+    print(out)
+    print('-----------------------------------------------------------------------------------------------------------------------')
+    with open(filename,mode='wb') as fo:
+        fo.write(out)
+                
+def encrypt_file_for_storage(filename, encrypted_file_handle, key):
+    print('encrypting:',filename)
+
+    with open(filename,'rb') as f:
+              bs = f.read()
+              ciphbytes = encrypt_data(bs,key)
+              print('-----------------encrypting file:',filename,'-----------------------')
+              encrypted_file_handle.write(ciphbytes)
+              print('---------------------------------------------------------------------------------')
+
+#------------------------------------------------------------------folder functions
 
 def get_next_directory_list_parsed_entry(z):
     #relative_file_name_length,relativepath, rolling_crypted_size_count, crypted_size, sz
@@ -571,65 +626,12 @@ def parse_file_list_string(header):
         entry,header = get_next_file_list_parsed_entry(header)
     return l  
 
-def decrypt_file_from_storage(data,prefix,relativepath,start,crypted_size,key):
-    #split
-    import os
-    #print(filename)
-    #print(os.path.dirname(filename))
-    print('prefix:\t',prefix)
-    print('path:\t',relativepath)
-    print('start:\t',start)
-    print('crypted_size\t',crypted_size)
-    filename = os.path.join(prefix,relativepath)
-    print('filename\t',filename)
-    try:
-        os.makedirs(os.path.dirname(filename))
-    except FileExistsError:
-        print("File already exists",filename)
-        pass
-    except FileNotFoundError:
-        print("File Not Found error",filename)
-        pass
-    out = decrypt_data(data[start:start+crypted_size],key)
-    print('-------------------------decrypted file:', relativepath, '----------------------------------------------')
-    print('unencrypted data',data[start:start+crypted_size])
-    print('start:',start)
-    #print('size:',sz)
-    print('crypted_size',crypted_size)
-    print(out)
-    print('-----------------------------------------------------------------------------------------------------------------------')
-    with open(filename,mode='wb') as fo:
-        fo.write(out)
-                
-def encrypt_file_for_storage(filename, encrypted_file_handle, key):
-    print('encrypting:',filename)
-
-    with open(filename,'rb') as f:
-              bs = f.read()
-              ciphbytes = encrypt_data(bs,key)
-              print('-----------------encrypting file:',filename,'-----------------------')
-              encrypted_file_handle.write(ciphbytes)
-              #print(ciphbytes)
-              print('---------------------------------------------------------------------------------')
-
-def get_header_string_val_from_bytes_in_array(t,i):
-    l = []
-    while t[i]!=0:
-        #print('-----------------------',sf[i])
-        #print(type(sf[i]))
-        #chr(sf[i])
-        #print('33')
-        l.append(chr(t[i]))
-        i = i + 1
-    i = i + 1
-    #print(l)
-    return int(''.join(l)),i
-
 def get_header(oldSelector, storage_file_name,key):
     with open(storage_file_name,'rb') as storage_file:
         sf = storage_file.read()
-        headerLength = decrypt_rounds_for_text_size(sf,key,sf[0:64])
-        text = text_decrypt_from_bytes_for_header(sf[0:32 + headerLength])
+        oldSelector = process_password(key)
+        headerLength = decrypt_rounds_for_text_size(oldSelector,key,sf[0:64])
+        text = text_decrypt_from_bytes_for_header(sf[0:16 + headerLength],key,)
         print('--------------------------header:',text)
         print(text)
         index = 0
@@ -659,7 +661,7 @@ def get_header(oldSelector, storage_file_name,key):
         
     return file_list_length,dirs_list_length,file_directory_length,list_of_files,list_of_dirs_string,total_encrypted_header_length
 
-#----------------------------------------------------------------folder functions------------------------
+
 def decrypt_folder(new_folder_name,prefix,storage_file_name,key):
     #TODO make empty directories
     #TODO figure out how to test for valid directories
@@ -669,9 +671,10 @@ def decrypt_folder(new_folder_name,prefix,storage_file_name,key):
     print('@@@@@@@@@@')
     #file_list_length,dirs_list_length,file_directory_length,list_of_files,list_of_dirs,total_encrypted_header_length+1
     file_list_length,dirs_list_length,file_directory_length,list_of_files,list_of_dirs,start_offset=result[0],result[1],result[2],result[3],result[4],result[5]
-    count = 0
+    count = start_offset
     print('count--------------',count)
     data = bytes([0])
+    '''
     #data,relative_file_name_length,relativepath,offset,sz
     
     with open(storage_file_name,'rb') as f:
@@ -679,6 +682,7 @@ def decrypt_folder(new_folder_name,prefix,storage_file_name,key):
         data = f.read()
         print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
     print(data)
+    '''
     for file_tuple in list_of_files:
         #start_pos, filename, filesize, encrypted_locker_filename, key
         
@@ -687,7 +691,7 @@ def decrypt_folder(new_folder_name,prefix,storage_file_name,key):
         #print('--------------------------------',new_folder_name+relativepath,start_offset)
         print('-------------------file tuple----------------------',file_tuple)
 
-        decrypt_file_from_storage(data, prefix, relativepath,count,crypted_size,key)
+        decrypt_file_from_storage(storage_file_name, prefix, relativepath,count,crypted_size,key)
         count = count + crypted_size
         #for i in range(10):
         #    decrypt_file_from_storage(start_offset+rolling_crypted_size_count-5+i, new_folder_name+relativepath+str(i), crypted_size, storage_file_name, key)
@@ -744,7 +748,7 @@ encryptOrDecrypt = input('(e)ncrypt or (d)ecrypt a folder or get (p)rint of both
 l=['fixed','other','per']
 
 #fix password to beyond 16 length
-text = b'abcdefghijklmnopqrstuvwxyz0123456789\r\nqwerasdfzxcv\r\nuiopjkl;,m/.\r\ntesttesttestzzzzzzzzzz'
+text = b'qwertyuiop[]asdfghjk;zxcvbnm,./12345678900987654321'
 ss = b'\xc3/:&\xcc4\xcc\xa7\xa2\xd8G\xec\xa9\x0f/\x0eC\x89\xa5W_$\xa2\xb0\x9b\x1e\xf9S\x107K\xa7\x0cA\x9cE\r\xb0\x01Q\xba\x11\xa4k\xb9\x81\xa8:\x80^\xcb\xe5\x865\x04OjD\xd0\x92h\xacK\xd5\xa9\x01\xf6\xa7\x94P\rM}WA\x7f\xaa\xdfk\xa42\x88S\x1eA\x1e\x88\x02\x10$B\x9aE\x03U\xe0'
 ss2 = b'C\xad:\x0cl\xac\xceR\xad\xa7`\x8e\x03\xceN\x8f\x02\x01\xa1|1\x13\xc1 &H"\xac\x1f\r\xaa\x04\xdb\xdc\xe0\xf8\xd0\xcbw\xa5}\x91\xa3\xa2\xbe:,\x9f\xde\xcc^\x8a\t\xa8NT\xf5tY&\xac\xa4\xd8\x9d@\x1c\xf6:X\x04p\xe6'
 
